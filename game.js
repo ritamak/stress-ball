ced3d5a94a04dd9ee314ac851f7d3baea7524203
ritamak <83585556+ritamak@users.noreply.github.com>
2021-06-12 09:29:48 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,97 +1,94 @@
+//Create my canvas
 const canvas = document.querySelector("#myCanvas");
 let ctx = canvas.getContext('2d');
 canvas.style.border = '2px solid black';
-ball(80,80);
-
 // Variaveis
-let count = 0
-let intervaloCores = setInterval(setColor, 500);
-let posicaoInicial = 0
-let circles = [{x:50,y:50}]
-let x = moveRandom(canvas.width);
-let y = moveRandom(canvas.height);
-console.log()
-let posicaoY = y
-
-console.log(moveBall())
-console.log(+moveRandom())
-
-function ball(x,y) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.save();
-  ctx.fillStyle = "white";
-  ctx.beginPath();
-  ctx.arc(x, y, 50, 0, 2 * Math.PI); 
-  ctx.fill();
-  ctx.strokeStyle = "white"
-  ctx.stroke()
-  ctx.closePath()
+let count = 0 // begin countdown
+let intervaloCores = setInterval(setColor, 1000); // change colors
+//Ball Class
+class Circle {
+    constructor(xpoint, ypoint, radius, color) {
+        this.xpoint = xpoint;
+        this.ypoint = ypoint;
+        this.radius = radius;
+        this.color = color;
+    }
+    //Draw a circle
+    draw(context) {
+        context.beginPath();
+        context.arc(this.xpoint, this.ypoint, this.radius, 0, Math.PI * 2, false)
+        context.strokeStyle = "white"
+        context.linewidth = 3
+        context.fillStyle = this.color
+        context.fill();
+        context.stroke();
+        context.closePath();
+    }
+    //Move the circle
+    moveBall(){
+        this.xpoint = moveRandom(canvas.width);
+        this.ypoint = moveRandom(canvas.height);  
+      }
+      //See User Click Event
+    clickCircle(xmouse, ymouse) {
+        const distance = 
+        Math.sqrt(
+            ( (xmouse - this.xpoint) * (xmouse - this.xpoint)) + ((ymouse - this.ypoint) * (ymouse - this.ypoint))
+        );
+        if (distance < this.radius) {
+            return console.log("Dentro da bola")
+        } else {
+            return console.log("Fora da bola")
+        }
+    
+    }
 }
-
-function moveBall(){  
-  let x = moveRandom(canvas.width);
-  console.log(x)
-  let y = moveRandom(canvas.height);
-  console.log(y)
-  ball(x, y);  
-}
-
+// Initialize my Ball
+let myBall = new Circle(100,100,50, "white")
+// Draw my ball
+myBall.draw(ctx)
+//Get a random number
 function moveRandom(axis) {
  return Math.floor(Math.random()*axis);
 }
-
-function level(dificulty, gameTime) {
-    let segundos = 0
-    if (dificulty === "easy") {
-        segundos = 1500
-    } else if(dificulty === "medium") {
-        segundos = 1000
+//Increase difficulty
+function level(difficulty, gameTime) {
+    let seconds = 0
+    if (difficulty === "easy") {
+        seconds = 1500
+    } else if (difficulty === "medium") {
+        seconds = 1000
     } else {
-        segundos = 900
+        seconds = 900
     }
     let id = setInterval(function() {
-        moveBall()
-    }, segundos)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        myBall.moveBall()
+        myBall.draw(ctx)
+    }, seconds)
     setTimeout(function(){ 
         clearInterval(id); 
     }, gameTime);
 }
-
+// Start Button
 function start() {
     level("easy", 15000)
     setTimeout( function() {level("medium", 10000)}, 15000 )
     setTimeout( function() {level("hard", 10000)}, 25000  )
-    
-   /* 
-    let ignoreClickOnMeElement = document.getElementById('someElementID');
-    document.addEventListener('click', function(event) {
-    var isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
-    if (!isClickInsideElement) {
-        //Do something click is outside specified element
-    }
-});*/
 }
-
+// Set background color
 function setColor() {
   canvas.style.backgroundColor = canvas.style.backgroundColor == "olive" ? "gold" : "olive";
 }
-
+// Stop button
 function stop() {
   clearInterval(intervaloCores);
   clearInterval(id)
 }
-function hit() {
-    count++
-    moveBall()
-}
-
-function gameOver(){
-
-}
-
-//innerText = 
-//// carrega para começar ; print time e result ;; parabens, o teu resultado é ......
-/// botao stop nao funciona 
-//// event listener mouse e de trackpad
-//// se nao carregar, gameover
-//// se carregar, ponto
+//EVENT LISTENER PARA O CLICK
+canvas.addEventListener("click", (event) => {
+    const c = canvas.getBoundingClientRect();
+    const x = event.clientX - c.left
+    const y = event.clientY - c.top
+    myBall.clickCircle(x,y)
+})
