ae357ab3147d2a6475a5d821e8ae830ef574fb00
ritamak <83585556+ritamak@users.noreply.github.com>
2021-06-12 09:30:22 +0100

    Add files via upload

---
+++ rascunhos.js
@@ -0,0 +1,11 @@
+/*
+Criar timer
+Criar contador de score;
+Countdown ate começar o jogo
+Fazer display da dificuldade durante o jogo
+Função acerteiNaBola() --- se acertar, desenhar outra bola e aumentar o score, se nao acertar, 
+fazer gameOver()
+Fazer tabela com resultados
+Restart button
+Por musica
+*/
